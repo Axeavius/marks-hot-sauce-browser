26cd09449c677df7efc2b891acf87dd7ac09e910
Mark Dunbar <axeavius@gmail.com>
2018-12-15 17:10:59 -0800

    Updates

---
+++ precache-manifest.ff4b414353cdf258ace80d6267ecbda2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a60fe088de80ad04e5f",
+    "url": "/marks-hot-sauce-browser/static/js/runtime~main.3a60fe08.js"
+  },
+  {
+    "revision": "27db2ab578a001496c4a",
+    "url": "/marks-hot-sauce-browser/static/js/main.27db2ab5.chunk.js"
+  },
+  {
+    "revision": "1d714d2257181748a59b",
+    "url": "/marks-hot-sauce-browser/static/js/1.1d714d22.chunk.js"
+  },
+  {
+    "revision": "27db2ab578a001496c4a",
+    "url": "/marks-hot-sauce-browser/static/css/main.0b474ed1.chunk.css"
+  },
+  {
+    "revision": "1541acd531664a0d8578945a8b78c8f6",
+    "url": "/marks-hot-sauce-browser/index.html"
+  }
+];
